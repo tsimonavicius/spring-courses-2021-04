883c8f3ac6d32f01f87de5746c42fc497a63c7e1
Tadas Simonavičius <tadas@inventi.lt>
2021-04-06 21:53:08 +0300

    1: Creating bean with Bean annotated method

--- di-example/src/main/java/lt/codeacademy/InternalMarksDao.java
+++ di-example/src/main/java/lt/codeacademy/InternalMarksDao.java
@@ -1,11 +1,7 @@
 package lt.codeacademy;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 
-@Component
 public class InternalMarksDao implements MarksDao {
 
     @Override
--- di-example/src/main/java/lt/codeacademy/SpringContextConfig.java
+++ di-example/src/main/java/lt/codeacademy/SpringContextConfig.java
@@ -1,9 +1,15 @@
 package lt.codeacademy;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class SpringContextConfig {
+
+    @Bean
+    public MarksDao marksDao() {
+        return new InternalMarksDao();
+    }
 }
