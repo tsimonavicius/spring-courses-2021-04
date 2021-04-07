33d23db4c303e92e6896cad634b80117d3de2170
Tadas Simonavičius <tadas@inventi.lt>
2021-04-07 19:16:06 +0300

    2: initial spring boot project

---
+++ eshop/src/test/java/lt/codeacademy/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
