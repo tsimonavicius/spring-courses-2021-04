ab8513db679256367b37b62caf5e79b9638349cb
Tadas Simonavičius <tadas@inventi.lt>
2021-04-12 19:22:21 +0300

    4: add initial data into DB

---
+++ eshop/src/main/resources/db/initial-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCTS (id, name, quantity, price, description) VALUES
+    ('acf49189-e872-48b0-949e-63d455f4fd86', 'iPhone XM', 200, 1599.99, 'Naujausias iPhone!'),
+    ('7a580a64-a414-40dc-8861-52aa79ff2259', 'Samsung S31', 50, 999.99, 'Pats geriausias Samsung!');
