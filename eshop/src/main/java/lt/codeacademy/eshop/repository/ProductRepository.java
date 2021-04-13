6fb068a136a63a8de3ce6712d7dca9ec8b8a45ae
Tadas Simonavičius <tadas@inventi.lt>
2021-04-13 19:11:19 +0300

    5: rename JPA repository

--- eshop/src/main/java/lt/codeacademy/eshop/repository/ProductRepository.java
+++ eshop/src/main/java/lt/codeacademy/eshop/repository/ProductRepository.java
@@ -1,57 +1,12 @@
 package lt.codeacademy.eshop.repository;
 
 import lt.codeacademy.eshop.model.Product;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
-/**
- * @author Andrius Baltrunas
- */
-@Repository
-public class ProductRepository {
-    private final Map<UUID, Product> products;
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 
-    private JdbcTemplate jdbcTemplate;
-
-    public ProductRepository(JdbcTemplate jdbcTemplate) {
-
-        this.jdbcTemplate = jdbcTemplate;
-        products = new HashMap<>();
-    }
-
-    public void save(Product product) {
-//        products.put(product.getId(), product);
-    }
-
-    public Product getProduct(UUID uuid) {
-        return products.get(uuid);
-    }
-
-    public List<Product> getProducts() {
-
-        return jdbcTemplate.query("SELECT * FROM Products", (rs, rowNum) -> {
-                Product product = new Product();
-
-                product.setId(UUID.fromString(rs.getString("id")));
-                product.setName(rs.getString("name"));
-                product.setQuantity(rs.getInt("quantity"));
-                product.setPrice(rs.getBigDecimal("price"));
-                product.setDescription(rs.getString("description"));
-
-                return product;
-            });
-    }
-
-    public void update(Product product) {
-//        products.put(product.getId(), product);
-    }
-
-    public void delete(UUID uuid) {
-        products.remove(uuid);
-    }
+    List<Product> findByName(String name);
 }
--- eshop/src/main/java/lt/codeacademy/eshop/service/ProductServiceImpl.java
+++ eshop/src/main/java/lt/codeacademy/eshop/service/ProductServiceImpl.java
@@ -1,7 +1,6 @@
 package lt.codeacademy.eshop.service;
 
 import lt.codeacademy.eshop.model.Product;
-import lt.codeacademy.eshop.repository.JPAProductRepository;
 import lt.codeacademy.eshop.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,45 +12,41 @@ import java.util.UUID;
  */
 @Service
 public class ProductServiceImpl implements ProductService {
-    private static final int MAX_COUNT = 10;
 
-    private final ProductRepository repository;
-    private JPAProductRepository jpaProductRepository;
+    private final ProductRepository productRepository;
 
-
-    public ProductServiceImpl(ProductRepository repository, JPAProductRepository jpaProductRepository) {
-        this.repository = repository;
-        this.jpaProductRepository = jpaProductRepository;
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     @Override
     public void addProduct(Product product) {
-        jpaProductRepository.save(product);
+        productRepository.save(product);
     }
 
     @Override
     public Product getProduct(UUID id) {
-        return jpaProductRepository.findById(id)
+        return productRepository.findById(id)
                 .orElseThrow(ProductNotFoundException::new);
     }
 
     @Override
     public List<Product> getProducts() {
-        return jpaProductRepository.findAll();
+        return productRepository.findAll();
     }
 
     @Override
     public void update(Product product) {
-        jpaProductRepository.save(product);
+        productRepository.save(product);
     }
 
     @Override
     public void delete(UUID id) {
-        jpaProductRepository.deleteById(id);
+        productRepository.deleteById(id);
     }
 
     @Override
     public Product getByName(String name) {
-        return jpaProductRepository.findByName(name).get(0);
+        return productRepository.findByName(name).get(0);
     }
 }
