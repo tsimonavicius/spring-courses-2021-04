0aa6caa3a1a92e7fe911d00bba837c8799b5f70d
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-04-14 22:12:10 +0300

    6 add language change script

---
+++ eshop/src/main/resources/static/js/language.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#languageSelector").change(function () {
+        var selectedOption = $('#languageSelector').val();
+        if (selectedOption != ''){
+            window.location.replace('?lang=' + selectedOption);
+        }
+    });
+})
