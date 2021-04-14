b0903336af249b93612117cd74bd626251cd96b2
Tadas Simonavičius <tadas@inventi.lt>
2021-04-14 08:08:45 +0300

    5: fix sorting for name by using ignoreCase

--- eshop/src/main/resources/db/initial-data.sql
+++ eshop/src/main/resources/db/initial-data.sql
@@ -8,4 +8,9 @@ INSERT INTO PRODUCTS (id, name, quantity, price, description) VALUES
     ('7a580a64-a414-40dc-8861-52aa79ff2255', 'Samsung S31', 50, 999.99, 'Pats geriausias Samsung!'),
     ('7a580a64-a414-40dc-8861-52aa79ff2256', 'Samsung S31', 50, 999.99, 'Pats geriausias Samsung!'),
     ('7a580a64-a414-40dc-8861-52aa79ff2257', 'Samsung S31', 50, 999.99, 'Pats geriausias Samsung!'),
+    ('7a580a64-a414-40dc-8861-52aa79ff2217', 'a', 50, 999.99, 'Pats geriausias Samsung!'),
+    ('7a580a64-a414-40dc-8861-52aa79ff2227', 'b', 50, 999.99, 'Pats geriausias Samsung!'),
+    ('7a580a64-a414-40dc-8861-52aa79ff1227', 'c', 50, 999.99, 'Pats geriausias Samsung!'),
+    ('7a580a64-a414-40dc-8861-52aa79ff3227', 'C', 50, 999.99, 'Pats geriausias Samsung!'),
+    ('7a580a64-a414-40dc-8861-52aa79ff4227', 'd', 50, 999.99, 'Pats geriausias Samsung!'),
     ('7a580a64-a414-40dc-8861-52aa79ff2258', 'Samsung S31', 50, 999.99, 'Pats geriausias Samsung!');
