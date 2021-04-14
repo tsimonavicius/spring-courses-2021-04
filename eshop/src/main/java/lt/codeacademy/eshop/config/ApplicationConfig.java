3fad0f82a30cc8131a2bd16f3b12d11e8c78562f
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-04-14 21:35:48 +0300

    6 add transalations

--- eshop/src/main/java/lt/codeacademy/eshop/config/ApplicationConfig.java
+++ eshop/src/main/java/lt/codeacademy/eshop/config/ApplicationConfig.java
@@ -1,7 +1,9 @@
 package lt.codeacademy.eshop.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
