c6e0ba3d74d40b0a84c48dea536b79ec3937edaf
Tadas Simonavičius <tadas@inventi.lt>
2021-04-19 20:34:13 +0300

    8: add custom attribute using ErrorViewResolver

--- eshop/src/main/java/lt/codeacademy/eshop/util/CustomErrorViewResolver.java
+++ eshop/src/main/java/lt/codeacademy/eshop/util/CustomErrorViewResolver.java
@@ -6,13 +6,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class CustomErrorViewResolver implements ErrorViewResolver {
 
     @Override
-    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
-        return new ModelAndView("customErrorPage", model);
+    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status,
+                                         Map<String, Object> model) {
+
+        Map<String, Object> customAttributes = new HashMap<>(model);
+        customAttributes.put("customAttribute", "Custom Attribute!");
+
+        return new ModelAndView("customErrorPage", customAttributes);
     }
 }
