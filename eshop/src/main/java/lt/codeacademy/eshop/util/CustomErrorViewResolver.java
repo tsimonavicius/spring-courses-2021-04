f21c8af8b44e72bab1dfbf02ffe6703af8510b7b
Tadas Simonavičius <tadas@inventi.lt>
2021-04-19 20:17:23 +0300

    8: create custom ErrorViewResolver

---
+++ eshop/src/main/java/lt/codeacademy/eshop/util/CustomErrorViewResolver.java
@@ -0,0 +1,18 @@
+package lt.codeacademy.eshop.util;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Component
+public class CustomErrorViewResolver implements ErrorViewResolver {
+
+    @Override
+    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+        return new ModelAndView("customErrorPage", model);
+    }
+}
