ccbb2a165b4c351b88f954149686ec72384386ca
Tadas Simonavičius <tadas@inventi.lt>
2021-04-20 18:48:13 +0300

    9: add String trim InitBinder

--- eshop/src/main/java/lt/codeacademy/eshop/controller/CustomControllerAdvice.java
+++ eshop/src/main/java/lt/codeacademy/eshop/controller/CustomControllerAdvice.java
@@ -1,9 +1,14 @@
 package lt.codeacademy.eshop.controller;
 
 import lt.codeacademy.eshop.exception.ProductNotFoundException;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.beans.PropertyEditor;
 
 @ControllerAdvice
 public class CustomControllerAdvice {
@@ -12,5 +17,11 @@ public class CustomControllerAdvice {
     public String productNotFound(ProductNotFoundException e, Model model) {
         model.addAttribute("productId", e.getProductId());
         return "productNotFound";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        PropertyEditor stringTrimmer = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmer);
     }
 }
