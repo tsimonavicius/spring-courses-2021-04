55ed3afc465c92a685dfd4808f587dd87230e1e3
Tadas Simonavičius <tadas@inventi.lt>
2021-04-26 21:44:17 +0300

    12: calculate total price

--- eshop/src/main/java/lt/codeacademy/eshop/model/Cart.java
+++ eshop/src/main/java/lt/codeacademy/eshop/model/Cart.java
@@ -2,6 +2,7 @@ package lt.codeacademy.eshop.model;
 
 import lombok.Getter;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,5 +14,14 @@ public class Cart {
     public void add(Product product) {
 
         cartItems.add(new CartItem(product));
+    }
+
+    public BigDecimal totalPrice() {
+
+        return cartItems.stream()
+//                [CartItem1, CartItem2]
+                .map(CartItem::price) // cartItem -> cartItem.price()
+                // [1, 2, 58, 55.99] --> 8952
+                .reduce(BigDecimal.ZERO, BigDecimal::add); // 0 + 1 = 1 + 2 = 3 + 58 = 61 + 55.99 = ... <--
     }
 }
