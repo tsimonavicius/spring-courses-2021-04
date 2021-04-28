63ec8236d81d59c780cf0fb98da2a4cd70e4fc9f
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-04-28 19:19:49 +0300

    14 paskaita add development profile

--- eshop/src/main/java/lt/codeacademy/eshop/EshopApplication.java
+++ eshop/src/main/java/lt/codeacademy/eshop/EshopApplication.java
@@ -9,14 +9,15 @@ import org.springframework.core.env.StandardEnvironment;
 public class EshopApplication {
 
 	public static void main(String[] args) {
-		ConfigurableEnvironment environment = new StandardEnvironment();
-		environment.setActiveProfiles("development");
+		// Set profile on application statUp
+		/*ConfigurableEnvironment environment = new StandardEnvironment();
+		environment.setActiveProfiles("production");
 
 		SpringApplication application = new SpringApplication(EshopApplication.class);
 		application.setEnvironment(environment);
-		application.run(args);
+		application.run(args);*/
 
-		//SpringApplication.run(EshopApplication.class, args);
+		SpringApplication.run(EshopApplication.class, args);
 	}
 
 }
