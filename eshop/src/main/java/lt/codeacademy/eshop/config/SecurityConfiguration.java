2828d7c9e55f1a5a858a6eccbe98a405382be171
Tadas Simonavičius <tadas@inventi.lt>
2021-05-03 19:17:27 +0300

    16: ignore security for h2 and static files

--- eshop/src/main/java/lt/codeacademy/eshop/config/SecurityConfiguration.java
+++ eshop/src/main/java/lt/codeacademy/eshop/config/SecurityConfiguration.java
@@ -1,20 +1,30 @@
 package lt.codeacademy.eshop.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @Profile("!test")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final String h2ConsolePath;
+
+    public SecurityConfiguration(@Value("${spring.h2.console.path:}") String h2ConsolePath) {
+        this.h2ConsolePath = h2ConsolePath;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        // buzinio logika ir app apsauga
         http
                 .authorizeRequests()
-                    .antMatchers("/prisijungimas", "/h2/**").permitAll()
+                    .antMatchers("/prisijungimas").permitAll()
                     .anyRequest()
                     .authenticated()
                     .and()
@@ -25,14 +35,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .usernameParameter("loginName")
                     .passwordParameter("loginPassword")
                     .defaultSuccessUrl("/products", true)
-                    .failureUrl("/prisijungimas?error")
-                    .and()
-                .csrf()
-                    .ignoringAntMatchers("/h2/**")
-                    .and()
-                .headers()
-                    .frameOptions()
-                    .sameOrigin();
+                    .failureUrl("/prisijungimas?error");
+    }
 
+    @Override
+    public void configure(WebSecurity web) {
+
+        // visa kita, ko mes nenorim apsaugoti
+        web.ignoring().requestMatchers(
+                PathRequest.toStaticResources().atCommonLocations()) // statiniai failai (css, images, js)
+                .antMatchers(h2ConsolePath + "/**"); // h2 console
     }
 }
