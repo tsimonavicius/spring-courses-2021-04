57645efd0f93dea635990bfad5e25ad30f83195e
Tadas Simonavičius <tadas@inventi.lt>
2021-05-04 21:28:05 +0300

    17: add Login button or user name and surname to navbar

--- eshop/src/main/java/lt/codeacademy/eshop/model/User.java
+++ eshop/src/main/java/lt/codeacademy/eshop/model/User.java
@@ -68,4 +68,8 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public String getFullName() {
+        return name + " " + surname;
+    }
 }
