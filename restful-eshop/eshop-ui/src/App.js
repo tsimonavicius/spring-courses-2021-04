d4bb48fcd0b5835a2c98538b19f449dc07ad089a
Tadas Simonavičius <tadas@inventi.lt>
2021-05-11 19:49:15 +0300

    21: create component and export using default

--- restful-eshop/eshop-ui/src/App.js
+++ restful-eshop/eshop-ui/src/App.js
@@ -1,10 +1,12 @@
+import HelloWorldComponent from "./components/HelloWorld";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello, World!</h1>
+			<HelloWorldComponent/>
+		</div>
+	);
 }
 
 export default App;
---
+++ restful-eshop/eshop-ui/src/components/HelloWorld.js
@@ -0,0 +1,5 @@
+function HelloWorld() {
+	return <h2>Hello, World, from Component!</h2>
+}
+
+export default HelloWorld;
