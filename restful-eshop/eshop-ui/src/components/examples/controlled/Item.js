f97742c0c431c7ee519f7400018d14b708f40f2c
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-05-12 21:56:54 +0300

    22 paskaita add badge component

--- restful-eshop/eshop-ui/src/components/examples/controlled/Item.js
+++ restful-eshop/eshop-ui/src/components/examples/controlled/Item.js
@@ -1,27 +1,13 @@
 import React from "react";
+import ItemBadge from "./ItemBadge";
 
 export default class Item extends React.Component {
-
-    getBadgeClass() {
-        let badgeClass = "m-2 badge bg-";
-        if (this.props.item.quantity > 2) {
-            badgeClass += "success";
-        } else if (this.props.item.quantity < 0) {
-            badgeClass += "danger";
-        } else {
-            badgeClass += "secondary";
-        }
-
-        return badgeClass;
-    }
-
     render() {
         return (
             <>
                 <div className="col-3">
                     <div className="input-group col-sm-6">
-                        <div className={this.getBadgeClass()}>{this.props.item.quantity}</div>
-
+                        <ItemBadge quantity={this.props.item.quantity}/>
                         <input type="text" className="form-control" placeholder="Recipient's username"
                                aria-label="Recipient's username with two button addons"
                                onChange={(event) => this.props.onHandlingItemText(event, this.props.item.id)}/>
