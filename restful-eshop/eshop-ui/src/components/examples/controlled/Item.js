3e5945cb265f32f9b30a07eadb04c47c777ffb93
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-05-12 20:41:22 +0300

    22 paskaita add text handler

--- restful-eshop/eshop-ui/src/components/examples/controlled/Item.js
+++ restful-eshop/eshop-ui/src/components/examples/controlled/Item.js
@@ -20,11 +20,21 @@ export default class Item extends React.Component {
         this.setState({quantity});
     }
 
+    onHandlingRemoveQty = () => {
+        const quantity = this.state.quantity - 1;
+        this.setState({quantity});
+    }
+
+    onHandlingItemText = (event) => {
+        const text = event.target.value;
+        this.setState( {text});
+    }
+
     getBadgeClass() {
-        let badgeClass= "m-2 badge bg-";
+        let badgeClass = "m-2 badge bg-";
         if (this.state.quantity > 2) {
             badgeClass += "success";
-        } else if(this.state.quantity < 0) {
+        } else if (this.state.quantity < 0) {
             badgeClass += "danger";
         } else {
             badgeClass += "secondary";
@@ -40,12 +50,18 @@ export default class Item extends React.Component {
                     <div className="input-group col-sm-6">
                         <div className={this.getBadgeClass()}>{this.state.quantity}</div>
                         <input type="text" className="form-control" placeholder="Recipient's username"
-                               aria-label="Recipient's username with two button addons"/>
+                               aria-label="Recipient's username with two button addons"
+                               onChange={this.onHandlingItemText}/>
                         <button className="btn btn-outline-secondary"
                                 type="button"
-                                onClick={this.onHandlingAddQuantity}>Add
+                                onClick={this.onHandlingAddQuantity}>+
+                        </button>
+                        <button className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={this.onHandlingRemoveQty}>-
                         </button>
                     </div>
+                    {this.state.text.length > 0 && <span>{this.state.text}</span>}
                 </div>
             </>
         );
