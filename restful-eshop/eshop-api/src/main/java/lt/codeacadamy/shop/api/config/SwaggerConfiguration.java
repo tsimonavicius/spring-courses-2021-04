bdc0802792474ffffd21d467ab237ee6dc40d207
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-05-17 18:29:37 +0300

    24 paskaita add swagger api information

--- restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/config/SwaggerConfiguration.java
+++ restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/config/SwaggerConfiguration.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 /**
  * @author Andrius Baltrunas
@@ -15,11 +19,23 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo("Eshop API",
+                "My custom Eshop API",
+                "1.0",
+                "Eshop API terms",
+                new Contact("Andrius Baltrunas", "www.codeacademy.lt", "andriusbaltrunas@codeacademy.lt"),
+                "Eshop API License",
+                "Eshop API URL",
+                Collections.emptyList());
     }
 }
