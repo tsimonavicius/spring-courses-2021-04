65a531e7166fc19673277c3c9bc36d5e065bd1c4
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-05-18 18:25:30 +0300

    25 paskaita add api doc

--- restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/controller/FIleController.java
+++ restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/controller/FIleController.java
@@ -1,11 +1,15 @@
 package lt.codeacadamy.shop.api.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lt.codeacadamy.shop.api.Endpoint;
 import lt.codeacadamy.shop.api.service.FileService;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,11 +28,24 @@ public class FIleController {
         this.fileService = fileService;
     }
 
+    @ApiOperation(value = "Save image", tags = "saveImage", httpMethod = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Kai sekmingai sukuriamas image"),
+            @ApiResponse(code = 403, message = "Neturit permisionu gauti atsakymas"),
+            @ApiResponse(code = 401, message = "Prisijunkite jei norit gauti atsakyma")
+    })
     @PostMapping(Endpoint.FILE_UPLOAD)
+    @ResponseStatus(HttpStatus.CREATED)
     public void saveFileInFileSystem(@RequestParam MultipartFile multipartFile) {
         fileService.saveFileInFileSystem(multipartFile);
     }
 
+    @ApiOperation(value = "Get image by name", tags = "getFileImages", httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Kai sekmingai grazinamas image"),
+            @ApiResponse(code = 403, message = "Neturit permisionu gauti atsakymas"),
+            @ApiResponse(code = 401, message = "Prisijunkite jei norit gauti atsakyma")
+    })
     @GetMapping(Endpoint.FILE_BY_NAME)
     public ResponseEntity<Resource> getFileByNameFromFileSystems(@PathVariable String name) {
 
