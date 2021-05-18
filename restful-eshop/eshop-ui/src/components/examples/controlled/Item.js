bdcbf9868ffb9ee22fd072607e486194ce17d195
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-05-18 22:04:01 +0300

    25 paskaita add emotion

--- restful-eshop/eshop-ui/src/components/examples/controlled/Item.js
+++ restful-eshop/eshop-ui/src/components/examples/controlled/Item.js
@@ -1,9 +1,12 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
 import React from "react";
 import ItemBadge from "./ItemBadge";
 import ItemText from "./ItemText";
 import "../../../styles/ItemActionStyles.css";
 import {Button, TextField} from "@material-ui/core";
 import {styled} from "@material-ui/styles";
+import {jsx, css} from "@emotion/react";
 
 const DeleteButton = styled(Button)({
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -29,13 +32,26 @@ export default class Item extends React.Component {
                                    onChange={(event) => this.props.onHandlingItemText(event, this.props.item.id)}/>
                         <Button className="addButton"
                                 onClick={() => this.props.onHandlingAddQuantity(this.props.item)}>+</Button>
-                        <Button className="removeButton"
-                                onClick={() => this.props.onHandlingRemoveQuantity(this.props.item)}>-</Button>
-                        <DeleteButton onClick={() => this.props.onHandlingDeleteItem(this.props.item.id)}>Delete</DeleteButton>
+
+
+                        <Button onClick={() => this.props.onHandlingRemoveQuantity(this.props.item)}
+                                css={css`
+                                     background-color: #6772e5;
+                                     color: #fff;
+                                     box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+                                     padding: 7px 14px;
+                                     &:hover {
+                                       background-color: #d48154;
+                                     }
+                                `}>-</Button>
+
+
+                        <DeleteButton
+                            onClick={() => this.props.onHandlingDeleteItem(this.props.item.id)}>Delete</DeleteButton>
                     </div>
                     {
                         this.props.item.isItemTextVisible && <ItemText item={this.props.item}
-                                  onHandlingHideText={this.props.onHandlingHideText}/>
+                                                                       onHandlingHideText={this.props.onHandlingHideText}/>
                     }
                 </div>
             </>
