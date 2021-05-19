c092138db4de68ca62d80cf8d77d34345bea3c76
Tadas Simonavičius <tadas@inventi.lt>
2021-05-19 19:36:52 +0300

    21: implement Footer

--- restful-eshop/eshop-ui/src/App.js
+++ restful-eshop/eshop-ui/src/App.js
@@ -4,10 +4,21 @@ import Content from "./components/Content";
 import Footer from "./components/Footer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+		flexDirection: 'column',
+		minHeight: '100vh',
+	}
+}));
 
 function App() {
+	const classes = useStyles()
+
 	return (
-		<div className="App container">
+		<div className={classes.root}>
 			<CssBaseline />
 			<Header/>
 			<Content/>
--- restful-eshop/eshop-ui/src/components/Footer/Footer.js
+++ restful-eshop/eshop-ui/src/components/Footer/Footer.js
@@ -1,4 +1,40 @@
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
 
-export default () => (
-	<h3>Footer</h3>
-)
+function Copyright() {
+	return (
+		<Typography variant="body2" color="textSecondary">
+			{'Copyright © '}
+			<Link color="inherit" href="https://material-ui.com/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
+}
+
+const useStyles = makeStyles((theme) => ({
+	footer: {
+		padding: theme.spacing(3, 2),
+		marginTop: 'auto',
+		backgroundColor:
+			theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+	}
+}));
+
+export default () => {
+	const classes = useStyles();
+
+	return (
+		<footer className={classes.footer}>
+			<Container maxWidth="sm">
+				<Typography variant="body1">Eshop Footer</Typography>
+				<Copyright/>
+			</Container>
+		</footer>
+	)
+}
