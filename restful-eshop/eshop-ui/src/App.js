f7665dae9585b1c1298951f40316221fdf4fb7bb
Tadas Simonavičius <tadas@inventi.lt>
2021-05-19 18:38:12 +0300

    21: create Header component skeleton

--- restful-eshop/eshop-ui/src/App.js
+++ restful-eshop/eshop-ui/src/App.js
@@ -1,23 +1,10 @@
-import HelloWorld from "./components/HelloWorld";
 import "./style.css"
-import ItemUncontrolledComponent from "./components/examples/uncontrolled/ItemUncontrolledComponent";
-import Item from "./components/examples/controlled/Item";
-import Items from "./components/examples/controlled/Items";
-import CreateProduct from "./components/examples/uncontrolled/CreateProduct";
-import Content from "./components/Content";
+import Header from "./components/Header/Header";
 
 function App() {
 	return (
 		<div className="App container">
-			{/*<h1>Hello, World!</h1>
-			<HelloWorld user={{
-				name: "World",
-				surname: "Surname"
-			}} />
-			<ItemUncontrolledComponent/>*/}
-			{/*<Items/>*/}
-			{/*<CreateProduct/>*/}
-			<Content/>
+			<Header/>
 		</div>
 	);
 }
---
+++ restful-eshop/eshop-ui/src/components/Header/Header.js
@@ -0,0 +1,4 @@
+
+export default () => (
+	<h1>Header</h1>
+)
