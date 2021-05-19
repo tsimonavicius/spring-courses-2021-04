b4cc9f66e73ab5fc76e02a1ff80333f14c7b3a21
Tadas Simonavičius <tadas@inventi.lt>
2021-05-19 19:12:18 +0300

    21: implement Header

--- restful-eshop/eshop-ui/src/App.js
+++ restful-eshop/eshop-ui/src/App.js
@@ -2,10 +2,13 @@ import "./style.css"
 import Header from "./components/Header/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import React from "react";
 
 function App() {
 	return (
 		<div className="App container">
+			<CssBaseline />
 			<Header/>
 			<Content/>
 			<Footer/>
