8fa7302c55d93089e9b1845d765301c55ad5b4ff
Tadas Simonavičius <tadas@inventi.lt>
2021-05-19 21:52:25 +0300

    21: add validation schema using Yup

--- restful-eshop/eshop-ui/src/components/ProductWithFormik/ProductWithFormik.js
+++ restful-eshop/eshop-ui/src/components/ProductWithFormik/ProductWithFormik.js
@@ -1,8 +1,28 @@
-import {Form, Formik, Field, ErrorMessage} from "formik"
-import {TextField} from "@material-ui/core";
+import {ErrorMessage, Field, Form, Formik} from "formik"
+import PropsState from "../PropsState/PropsState";
+import * as Yup from "yup"
+
+const validationSchema = Yup.object().shape({
+	name: Yup.string()
+		.min(2, "Reiksme turi buti ilgesne nei 2 characteriai")
+		.required(),
+	price: Yup.number()
+		.positive()
+		.required(),
+	quantity: Yup.number()
+		.positive()
+		.required(),
+	description: Yup.string()
+		.max(100)
+})
 
 export default () => (
-	<Formik initialValues={{name: ''}}
+	<Formik initialValues={{
+				name: '',
+				price: '',
+				quantity: '',
+				description: ''
+			}}
 			onSubmit={(values, helpers) => {
 				console.log("values", values)
 				console.log("helpers", helpers)
@@ -13,24 +33,38 @@ export default () => (
 					helpers.setSubmitting(false)
 				}, 3000)
 			}}
-			validate={values => {
-				const errors = {}
-
-				if (values.name.length < 5) {
-					errors.name = "VAi vaiva vaia"
-				}
-
-				return errors
-			}}>
+		validationSchema={validationSchema}>
 		{props => (
-			<Form>
-				<label htmlFor="name"/>
-				<Field id="name" name="name" placeholder="Type..." component={TextField}/>
-				<ErrorMessage name="name" component="div"/>
+			<>
+				<PropsState {...props} />
+				<Form>
+					<div>
+						<label htmlFor="name">Product title:</label>
+						<Field id="name" name="name" placeholder="Type..."/>
+						<ErrorMessage name="name" component="span"/>
+					</div>
 
-				{!props.isSubmitting ? <button type="submit">Submit</button> : <span>Submitting...</span>}
-			</Form>
-		)
-		}
+					<div>
+						<label htmlFor="price">Product price:</label>
+						<Field id="price" name="price" placeholder="Type..."/>
+						<ErrorMessage name="price" component="span"/>
+					</div>
+
+					<div>
+						<label htmlFor="quantity">Product quantity:</label>
+						<Field id="quantity" name="quantity" placeholder="Type..."/>
+						<ErrorMessage name="quantity" component="span"/>
+					</div>
+
+					<div>
+						<label htmlFor="description">Product quantity:</label>
+						<Field id="description" name="description" placeholder="Type..." component="textarea"/>
+						<ErrorMessage name="description" component="span"/>
+					</div>
+
+					{!props.isSubmitting ? <button type="submit">Submit</button> : <span>Submitting...</span>}
+				</Form>
+			</>
+		)}
 	</Formik>
 )
---
+++ restful-eshop/eshop-ui/src/components/PropsState/PropsState.js
@@ -0,0 +1,15 @@
+export default (props) => (
+	<div style={{margin: '1rem 0'}}>
+		<h3 style={{fontFamily: 'monospace'}}/>
+		<pre
+			style={{
+				background: '#f6f8fa',
+				fontSize: '1rem',
+				padding: '.5rem',
+			}}
+		>
+                    <strong>props</strong> ={' '}
+			{JSON.stringify(props, null, 2)}
+                    </pre>
+	</div>
+)
