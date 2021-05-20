0280acd5fda008f214f6471e1e1354b0cd3db1f9
Tadas Simonavičius <tadas@inventi.lt>
2021-05-20 21:20:32 +0300

    27: fix labels

--- restful-eshop/eshop-ui/src/components/FormikInput/FormikInput.js
+++ restful-eshop/eshop-ui/src/components/FormikInput/FormikInput.js
@@ -1,10 +1,11 @@
-import {FormControl, FormHelperText, FormLabel, InputLabel, OutlinedInput} from "@material-ui/core";
+import {FormControl, FormHelperText, InputLabel, OutlinedInput} from "@material-ui/core";
 import {ErrorMessage, Field} from "formik";
 
 const FormikInput = ({name, label, error, placeholder = "Type...", ...props}) => (
 	<FormControl error={error} fullWidth variant="outlined" margin="dense">
-		<FormLabel htmlFor={name}>{label}</FormLabel>
-		<Field id={name} name={name} placeholder={placeholder} {...props} as={OutlinedInput}/>
+		<InputLabel htmlFor={name}>{label}</InputLabel>
+		<Field id={name} name={name} label={label}
+			   placeholder={placeholder} {...props} as={OutlinedInput}/>
 		<ErrorMessage name={name} component={FormHelperText}/>
 	</FormControl>
 )
