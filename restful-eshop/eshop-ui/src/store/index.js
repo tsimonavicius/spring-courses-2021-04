89c10e00b1b02f248faedb1ba11a4e8cbf77d25e
Tadas Simonavičius <tadas@inventi.lt>
2021-05-27 19:38:34 +0300

    31: add Redux Logger middleware

--- restful-eshop/eshop-ui/src/store/index.js
+++ restful-eshop/eshop-ui/src/store/index.js
@@ -1,5 +1,6 @@
-import {combineReducers, compose, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import cartReducer from "./slices/cartSlice";
+import {createLogger} from "redux-logger";
 
 /* store = {
 		cart: [...],
@@ -17,7 +18,11 @@ const constructStore = () => {
 
 	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-	return createStore(rootReducer, undefined, composeEnhancers())
+	return createStore(
+		rootReducer,
+		undefined,
+		composeEnhancers(applyMiddleware(createLogger()))
+	)
 }
 
 export default constructStore
