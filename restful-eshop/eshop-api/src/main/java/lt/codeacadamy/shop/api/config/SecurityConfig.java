1124619565bec61d92cb904f0f2328e86ff2306c
Tadas Simonavičius <tadas@inventi.lt>
2021-05-31 20:34:23 +0300

    32: enable Spring Security without Session and CSRF, all paths are protected

---
+++ restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package lt.codeacadamy.shop.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .exceptionHandling()
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
+    }
+}
