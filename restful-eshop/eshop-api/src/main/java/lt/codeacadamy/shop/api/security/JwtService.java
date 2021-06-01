d821062627141938e7e985c6c5f898ff17b8417d
Tadas Simonavičius <tadas@inventi.lt>
2021-06-01 20:06:20 +0300

    33: create JWT parse method

--- restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/security/JwtService.java
+++ restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/security/JwtService.java
@@ -1,15 +1,24 @@
 package lt.codeacadamy.shop.api.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lt.codeacadamy.shop.api.entity.Role;
 import lt.codeacadamy.shop.api.entity.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
 @Service
@@ -38,8 +47,25 @@ public class JwtService {
                 .setIssuedAt(now)
                 .claim("roles", user.getRoles().stream()
                     .map(Role::getName)
-                    .collect(toSet()))
+                    .collect(toSet())) // ["ADMIN", "USER"]
                 .signWith(Keys.hmacShaKeyFor(secretKey),SignatureAlgorithm.HS512)
                 .compact();
+    }
+
+    public Authentication parseToken(String jwt) {
+
+        // parse and validate JWT
+        Claims parsedJwtBody = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJwt(jwt)
+                .getBody();
+
+        String username = parsedJwtBody.getSubject();
+        List<GrantedAuthority> roles = ((List<String>) parsedJwtBody.get("roles")).stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(toList());
+
+        return new UsernamePasswordAuthenticationToken(username, null, roles);
     }
 }
