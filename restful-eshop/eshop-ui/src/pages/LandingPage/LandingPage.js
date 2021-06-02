e6197ab21aed26d9337ff156ec290915a0d13618
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-06-02 19:47:28 +0300

    34 paskaita add Trans example

--- restful-eshop/eshop-ui/src/pages/LandingPage/LandingPage.js
+++ restful-eshop/eshop-ui/src/pages/LandingPage/LandingPage.js
@@ -1,23 +1,16 @@
-import {Translation} from "react-i18next";
-import i18n from "i18next";
+import {Trans} from "react-i18next";
 
 const LandingPage = () => {
 
 	return (
 		<>
-			<Translation>
-				{(t, {i18n}) =>
-					<>
-						<h1>Hello!</h1>
-						<div>{t('Welcome to key not found')}</div>
-						<div>{t('Key with param', {name: 'Andrius', surname: 'Baltrunas'})}</div>
-						<div>{t('second:Key', {count: 0})}</div>
-						<div>{t('products:Key2')}</div>
-					</>
-				}
-			</Translation>
+			<Trans
+				i18nKey="Key with param2"
+				defaults="This is default value"
+				values={{name:'Andrius', surname: 'Baltrunas'}}
+			/>
 		</>
 	)
-};
+}
 
 export default LandingPage;
