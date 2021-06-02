12f0ddc22d67b3e15d7dcf7a78ddfb4aba4fc0c0
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-06-02 19:10:00 +0300

    34 paskaita add json file

--- restful-eshop/eshop-ui/src/i18n/i18n.js
+++ restful-eshop/eshop-ui/src/i18n/i18n.js
@@ -1,15 +1,12 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
+import en from "./translations/en.json";
 
 i18n
     .use(initReactI18next)
     .init({
         resources: {
-            en: {
-                translation: {
-                    "key": "Welcome to React"
-                }
-            }
+            en
         },
         lng: "en",
         fallbackLng: "en",
--- restful-eshop/eshop-ui/src/pages/LandingPage/LandingPage.js
+++ restful-eshop/eshop-ui/src/pages/LandingPage/LandingPage.js
@@ -1,5 +1,14 @@
-const LandingPage = () => (
-	<h1>Hello!</h1>
-)
+import {useTranslation} from "react-i18next";
+
+const LandingPage = () => {
+	const {t} = useTranslation();
+
+	return (
+		<>
+			<h1>Hello!</h1>
+			{t('Welcome to key not found')}
+		</>
+	)
+}
 
 export default LandingPage
