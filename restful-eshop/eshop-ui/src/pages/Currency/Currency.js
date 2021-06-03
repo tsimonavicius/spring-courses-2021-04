87e7814d7f507ccba82b6b2fcce3f7760e0f3e4e
Andrius Baltrunas <andrius.baltrunas@3ds.com>
2021-06-03 20:00:08 +0300

    35 paskaita styled component

--- restful-eshop/eshop-ui/src/pages/Currency/Currency.js
+++ restful-eshop/eshop-ui/src/pages/Currency/Currency.js
@@ -1,9 +1,31 @@
 import {useEffect, useState} from "react";
 import {fetchCurrencies} from "../../api/CurrencyApi";
+import {
+    CircularProgress,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import {makeStyles} from "@material-ui/styles";
+import {useTranslation} from "react-i18next";
+
+const useStyle = makeStyles({
+    table: {
+        minWidth: 450,
+    }
+})
 
 const Currency = () => {
     const [currencies, setCurrencies] = useState();
     const [loading, setLoading] = useState(true);
+
+    const {t} = useTranslation('currency');
 
     useEffect(() => {
         fetchCurrencies().then(({data}) => {
@@ -11,20 +33,36 @@ const Currency = () => {
         }).finally(() => setLoading(false))
     }, []);
 
+    const classes = useStyle();
+
     return (
         <>
-            {
-                loading ? <span>Loading...</span> :
-                    <div>
-                        <div>{currencies.base}</div>
-                        <div>{currencies.date}</div>
-                        {
-                            Object.entries(currencies.rates).map(([key, value]) => (
-                                <div>{key}: {value}</div>
-                            ))
-                        }
-                    </div>
-            }
+        {loading ? <CircularProgress/> :
+                <Container maxWidth="md">
+                    <TableContainer component={Paper} className={classes.table}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>{currencies.base}</TableCell>
+                                    <TableCell>{t('value')}</TableCell>
+                                    <TableCell>{t('currency')}</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    Object.entries(currencies.rates).map(([key, value]) => (
+                                        <TableRow>
+                                            <TableCell>1</TableCell>
+                                            <TableCell>{value}</TableCell>
+                                            <TableCell>{key}</TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Container>
+        }
         </>
     );
 }
