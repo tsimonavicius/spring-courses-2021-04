b972d364ead1fb4ad2d78c32132b8b643a527a6e
Tadas Simonavičius <tadas@inventi.lt>
2021-06-07 19:13:34 +0300

    36: make weather API endpoint public

--- restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/config/SecurityConfig.java
+++ restful-eshop/eshop-api/src/main/java/lt/codeacadamy/shop/api/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     .and()
                 .authorizeRequests()
+                    .antMatchers("/weather").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .exceptionHandling()
