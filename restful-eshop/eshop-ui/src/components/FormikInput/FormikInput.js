0c8783d50da44100bd19e559223eda922120f657
Tadas Simonavičius <tadas@inventi.lt>
2021-06-10 21:45:36 +0300

    39: validation messages translation

--- restful-eshop/eshop-ui/src/components/FormikInput/FormikInput.js
+++ restful-eshop/eshop-ui/src/components/FormikInput/FormikInput.js
@@ -1,13 +1,23 @@
 import {FormControl, FormHelperText, InputLabel, OutlinedInput} from "@material-ui/core";
 import {ErrorMessage, Field} from "formik";
+import {useTranslation} from "react-i18next";
 
-const FormikInput = ({name, label, error, placeholder = "Type...", ...props}) => (
-	<FormControl error={error} fullWidth variant="outlined" margin="dense">
-		<InputLabel htmlFor={name}>{label}</InputLabel>
-		<Field id={name} name={name} label={label}
-			   placeholder={placeholder} {...props} as={OutlinedInput}/>
-		<ErrorMessage name={name} component={FormHelperText}/>
-	</FormControl>
-)
+const FormikInput = ({name, label, error, placeholder = "Type...", ...props}) => {
+
+	const { t, i18n } = useTranslation()
+
+	return (
+		<FormControl error={error} fullWidth variant="outlined" margin="dense" key={i18n.language}>
+			<InputLabel htmlFor={name}>{label}</InputLabel>
+			<Field id={name} name={name} label={label}
+				   placeholder={placeholder} {...props} as={OutlinedInput}/>
+			<ErrorMessage name={name}>
+				{
+					msg => <FormHelperText>{t(msg)}</FormHelperText>
+				}
+			</ErrorMessage>
+		</FormControl>
+	)
+}
 
 export default FormikInput
--- restful-eshop/eshop-ui/src/components/UserRegistration/UserRegistration.js
+++ restful-eshop/eshop-ui/src/components/UserRegistration/UserRegistration.js
@@ -4,14 +4,16 @@ import {Paper} from "@material-ui/core";
 import FormikInput from "../FormikInput/FormikInput";
 import Button from "@material-ui/core/Button";
 import * as Yup from "yup";
+import {useTranslation} from "react-i18next";
+
 const validationSchema = Yup.object().shape({
 	name: Yup.string()
-		.required(),
+		.required("validations:required"),
 	surname: Yup.string()
-		.required(),
+		.required("validations:required"),
 	email: Yup.string()
 		.email()
-		.required(),
+		.required("validations:required"),
 	password: Yup.string()
 		.min(5)
 		.required(),
@@ -21,60 +23,65 @@ const validationSchema = Yup.object().shape({
 		.oneOf([Yup.ref('password')], "Blogas")
 })
 
-export default () => (
-	<Formik initialValues={{
-		name: '',
-		surname: '',
-		email: '',
-		password: '',
-		repeatPassword: ''
-	}}
-			validationSchema={validationSchema}>
-		{props => (
-			<>
-				<Container maxWidth={"sm"}>
-					<Paper elevation={3}>
-						<Form style={{margin: 20}}>
-							<div>
-								<FormikInput name="name"
-											 label="Name"
-											 error={props.touched.name && !!props.errors.name}
-											 placeholder="Name"/>
-							</div>
-							<div>
-								<FormikInput name="surname"
-											 label="Surname"
-											 error={props.touched.surname && !!props.errors.surname}
-											 placeholder="Surname"/>
-							</div>
-							<div>
-								<FormikInput name="email"
-											 label="Email"
-											 error={props.touched.email && !!props.errors.email}
-											 placeholder="Email"/>
-							</div>
-							<div>
-								<FormikInput name="password"
-											 label="Password"
-											 error={props.touched.password && !!props.errors.password}
-											 placeholder="Password"
-											 type="password"/>
-							</div>
-							<div>
-								<FormikInput name="repeatPassword"
-											 label="RepeatPassword"
-											 error={props.touched.repeatPassword && !!props.errors.repeatPassword}
-											 placeholder="repeatPassword"
-											 type="password"/>
-							</div>
-							{!props.isSubmitting ?
-								<Button variant="contained" color="primary" type="submit">Submit</Button>
-								:
-								<span>Submitting...</span>}
-						</Form>
-					</Paper>
-				</Container>
-			</>
-		)}
-	</Formik>
-)
+export default () => {
+
+	const { t, i18n } = useTranslation()
+
+	return (
+		<Formik initialValues={{
+			name: '',
+			surname: '',
+			email: '',
+			password: '',
+			repeatPassword: ''
+		}}
+				validationSchema={validationSchema}>
+			{props => (
+				<>
+					<Container maxWidth={"sm"}>
+						<Paper elevation={3}>
+							<Form style={{margin: 20}}>
+								<div>
+									<FormikInput name="name"
+												 label={t("login:email")}
+												 error={props.touched.name && !!props.errors.name}
+												 placeholder="Name"/>
+								</div>
+								<div>
+									<FormikInput name="surname"
+												 label="Surname"
+												 error={props.touched.surname && !!props.errors.surname}
+												 placeholder="Surname"/>
+								</div>
+								<div>
+									<FormikInput name="email"
+												 label="Email"
+												 error={props.touched.email && !!props.errors.email}
+												 placeholder="Email"/>
+								</div>
+								<div>
+									<FormikInput name="password"
+												 label="Password"
+												 error={props.touched.password && !!props.errors.password}
+												 placeholder="Password"
+												 type="password"/>
+								</div>
+								<div>
+									<FormikInput name="repeatPassword"
+												 label="RepeatPassword"
+												 error={props.touched.repeatPassword && !!props.errors.repeatPassword}
+												 placeholder="repeatPassword"
+												 type="password"/>
+								</div>
+								{!props.isSubmitting ?
+									<Button variant="contained" color="primary" type="submit">Submit</Button>
+									:
+									<span>Submitting...</span>}
+							</Form>
+						</Paper>
+					</Container>
+				</>
+			)}
+		</Formik>
+	)
+}
